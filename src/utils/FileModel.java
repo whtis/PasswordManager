42b85207910dec3bae25c00c4e3fea5384ba61f8
whtisw <whtisw@gmail.com>
2016-02-24 01:12:57 +0800

    莫名其妙的报错，待解决。。。

---
+++ src/utils/FileModel.java
@@ -0,0 +1,25 @@
+package utils;
+
+import javax.swing.*;
+
+/**
+ * Created by ht on 2016/2/24.
+ */
+public class FileModel extends AbstractListModel {
+
+    private String[] files;
+
+    public FileModel(String[] files) {
+        this.files = files;
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        return (index + 1) + "." + files[index++];
+    }
+
+    @Override
+    public int getSize() {
+        return files.length;
+    }
+}
